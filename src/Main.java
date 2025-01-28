443edabcf1ce50c77e6735a27003c90576bc0a07
Yupi <youngperchik2404@gmail.com>
2025-01-28 18:40:13 +0500

    created an oop calculator

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать в калькулятор!!");
+        System.out.println("Выберите действие: ");
+        System.out.println("1. Сложение (add)\n2. Вычитание (subtract)\n3. Умножение (multiply)\n4. деление (divide)\n5. Возведение в степень (pow)\n6. Вычисление факториала (factorial)\n");
+
+        int choose = input.nextInt();
+
+        System.out.print("\n\nВведите 1 число: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("\n\nВведите 2 число: ");
+        double num2 = input.nextDouble();
+
+        double funcRes = 0;
+
+        try {
+            FileWriter writer = new FileWriter("input.txt", true);
+
+            writer.write((int) num1);
+
+            writer.write((int) num2);
+
+            writer.close();
+        }
+        catch (IOException e){
+            System.out.println("Возникла ошибка во время записи, проверьте данные.");
+        }
+
+        if (choose == 1){
+            funcRes =  Calculator.Add(num1, num2);
+        }
+
+        else if (choose == 2) {
+            funcRes =  Calculator.Substract(num1, num2);
+        }
+
+        else if (choose == 3) {
+            funcRes =  Calculator.Multiply(num1, num2);
+        }
+
+        else if (choose == 4) {
+            try {
+                double result = Calculator.Divide(num1, num2);
+                funcRes = result;
+            }
+            catch (ArithmeticException e){
+                System.out.println("Ошибка: Деление на ноль невозможно.");
+            }
+        }
+
+        else if (choose == 5) {
+            funcRes =  ScientificCalculator.Pow(num1, num2);
+        }
+
+        else if (choose == 6) {
+            funcRes =  ScientificCalculator.Factorial(num1);
+        }
+
+        try {
+            FileWriter writer = new FileWriter("output.txt", true);
+
+            writer.write((int) funcRes);
+
+            writer.close();
+        }
+        catch (IOException e){
+            System.out.println("Возникла ошибка во время записи, проверьте данные.");
+        }
+    }
+}
---
+++ src/ScientificCalculator.java
@@ -0,0 +1,14 @@
+public class ScientificCalculator {
+    public static double Pow(double num1, double num2){
+        return Math.pow(num1, num2);
+    }
+
+    public static double Factorial(double num1){
+        if (num1 == 0 || num1 == 0){
+            return 1;
+        }
+        else {
+            return num1 * Factorial(num1 - 1);
+        }
+    }
+}
